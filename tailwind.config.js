440a9770dd2e19ac0cba7234cd421626824d6dcf
Jakir Shorkar <shorkarjakir@gmail.com>
2025-01-02 21:54:47 +0600

    navar completd

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            'primay-btn': 'B4F461',
+          },
+      },
+    },
+    plugins: [],
+  }
